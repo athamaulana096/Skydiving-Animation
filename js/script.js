7c87135e8eda3206fdb8b82997193f410f0f945d
athamaulana096 <athamaulana30@gmail.com>
2023-07-17 15:21:26 +0700

    Membuat Awan
    Awan sudah diacak posisinya.
    Mengacak awan mana yang tembus ke pesawat.
    
    Note: timing dari kecepatan pesawat dan terjunnya Sky Diver bisa dibuat dengan sesuka hati, serta jumlah awan yang tampil.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,43 @@
 const plane = document.getElementById('plane');
 const skydiver = document.getElementById('skydiver');
+const canvas = document.getElementById('canvas');
+const clouds = document.getElementById('clouds');
+const totalClouds = 50;
+
+//random min-max
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function setClouds() {
+  for (let i = 1; i <= totalClouds; i++) {
+    let cloud = document.createElement('div');
+    cloud.id = 'cloud' + i;
+    cloud.classList.add('cloud' + random(1, 3));
+    clouds.appendChild(cloud);
+
+    //mengacak posisi awan
+    cloud.style.left = random(-20, -window.innerWidth * 2) + 'px';
+    cloud.style.top = random(0, window.innerHeight) + 'px';
+    cloud.style.zIndex = random(1, 10);
+  }
+}
+
+function setBG() {
+  canvas.style.width = window.innerWidth + 'px';
+  canvas.style.height = window.innerHeight * 2 + 'px';
+  canvas.style.top = -window.innerHeight + 'px';
+}
 
 function animate() {
   plane
     .velocity(
       {
-        right: '950px',
-        bottom: '5%',
+        right: '30vh',
         transform: ['rotate(10deg)', 'rotate(1deg)'],
       },
       {
-        duration: 4000,
+        duration: 2000,
       }
     )
     .velocity(
@@ -19,7 +46,7 @@ function animate() {
         left: '40%',
       },
       {
-        duration: 5000,
+        duration: 8000,
       }
     )
     .velocity(
@@ -63,6 +90,34 @@ function animate() {
         duration: 10000,
       }
     );
+
+  canvas.velocity(
+    {
+      top: '0px',
+    },
+    {
+      queue: false,
+      duration: 11000,
+    }
+  );
+
+  for (let i = 1; i <= totalClouds; i++) {
+    let duration = Math.abs(parseInt(document.getElementById('cloud' + i).style.left) / 100) * 2000;
+
+    if (duration < 10000) {
+      duration = random(10000, 15000);
+    }
+    document.getElementById('cloud' + i).velocity(
+      {
+        left: window.innerWidth,
+      },
+      {
+        duration: duration,
+      }
+    );
+  }
 }
 
+setClouds();
+setBG();
 animate();
